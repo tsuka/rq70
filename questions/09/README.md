7d81a4df2091f8324500f2e9ad3aee9b23885eb9
kayhide <kayhide@gmail.com>
2016-06-14 20:37:08 +0900

    create q09.

---
+++ questions/09/README.md
@@ -0,0 +1,9 @@
+# questions/09
+
+```sh
+% ruby main.rb
+       user     system      total        real
+  79.430000   1.990000  81.420000 ( 82.091184)
+
+answer : 2417416
+```
---
+++ questions/09/main.rb
@@ -0,0 +1,47 @@
+require 'benchmark'
+require 'pry'
+
+module Q09
+  MALE = 20
+  FEMALE = 10
+  # MALE = 10
+  # FEMALE = 5
+
+  module_function
+
+  def patterns states
+    if done? states.last
+      [states]
+    elsif valid? states.last
+      patterns(states + [[states.last[0] + 1, states.last[1]]]) +
+        patterns(states + [[states.last[0], states.last[1] + 1]])
+    else
+      []
+    end
+  end
+
+  def valid? state
+    (state == [0, 0]) ||
+      (
+        (state[0] <= MALE) && (state[1] <= FEMALE) &&
+        (state[0] != state[1]) && (MALE - state[0] != FEMALE - state[1])
+      )
+  end
+
+  def done? state
+    state == [MALE, FEMALE]
+  end
+
+  def run
+    patterns([[0, 0]]).count
+  end
+end
+
+Benchmark.bm do |x|
+  x.report do
+    $answer = Q09.run
+  end
+end
+
+puts
+puts "answer : #{$answer}"
